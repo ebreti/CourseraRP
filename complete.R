c3cea3ddbdc2cea1f432a3aa22909e7dd86023db
Flavio M Carvalho <contato.ebreti@gmail.com>
2014-06-12 01:47:06 -0300

    Week 2 c02
    script pollutatmean corrigido para submissão
    script complet submetido

--- complete.R
+++ complete.R
@@ -2,14 +2,55 @@ complete <- function(directory, id = 1:332) {
   ## 'directory' is a character vector of length 1 indicating
   ## the location of the CSV files
   
+  idwk <- numeric(1)
+  idwk <- NA
+  nobs <- 0
+  result <- data.frame(idwk, nobs)
+  dirOK <- FALSE
+  if(file.exists(directory)) {
+    if(file.info(directory)$isdir) {
+      dirOK <- TRUE
+    }
+  }
+  if(!dirOK) {
+    print(paste("not a directory ::",directory))
+  }
+
   ## 'id' is an integer vector indicating the monitor ID numbers
   ## to be used
   
-  ## Return a data frame of the form:
+  if(dirOK){
+    fileNF <- vector(mode="character")
+    for(idmon in id) {
+      idm3 <- paste("000",idmon,sep="")
+      file <- paste(substr(idm3, nchar(idm3)-3+1, nchar(idm3)),".csv",sep="")
+      fullname <- paste(directory,file,sep="/")
+      if(file.exists(fullname)) {
+        monitor_data <- read.csv(fullname)
+        if(! is.na(result$idwk[nrow(result)])) {
+          idwk <- NA
+          nobs <- 0
+          result <- rbind(result, data.frame(idwk, nobs)) 
+        }
+        result$idwk[nrow(result)] <- idmon
+        for(line in 1:nrow(monitor_data)) {
+          if(! is.na(monitor_data$sulfate[line]) && ! is.na(monitor_data$nitrate[line])) {
+            nobs <- nobs + 1
+          }
+        }
+        result$nobs[nrow(result)] <- nobs
+      } else {
+        fileNF[length(fileNF)+1] <- file
+      }
+    }
+  }  
+    ## Return a data frame of the form:
   ## id nobs
   ## 1  117
   ## 2  1041
   ## ...
   ## where 'id' is the monitor ID number and 'nobs' is the
   ## number of complete cases
+  colnames(result) <- c("id", "nobs")
+  result
 }
--- pollutantmean.R
+++ pollutantmean.R
@@ -2,6 +2,7 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   ## 'directory' is a character vector of length 1 indicating
   ## the location of the CSV files
   
+  result <- NA
   dirOK <- FALSE
   if(file.exists(directory)) {
     if(file.info(directory)$isdir) {
@@ -35,7 +36,7 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
       idm3 <- paste("000",idmon,sep="")
       file <- paste(substr(idm3, nchar(idm3)-3+1, nchar(idm3)),".csv",sep="")
       fullname <- paste(directory,file,sep="/")
-      if(file.exists(fullname)) {
+      if(file.exists(fullname) && pollOK) {
         monitor_data <- read.csv(fullname)
         measures <- c(measures, monitor_data[[pollutant]])
       } else {
@@ -46,10 +47,11 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
     ## Return the mean of the pollutant across all monitors list
     ## in the 'id' vector (ignoring NA values)
 
-    print(mean(measures,na.rm=TRUE))
+    result <- mean(measures,na.rm=TRUE)
     if(length(fileNF) > 0) {
       print("monitors not found")
       print(fileNF)
     }
   }
+  result
 }
